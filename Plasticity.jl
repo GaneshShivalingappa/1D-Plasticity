6a516c200a46f81d4f01522b4e51bd3ad6687a5f
Ganesh Shivalingappa <ganesh.shivalingappa@tu-braunschweig.de>
2024-07-16 21:27:19 +0200

    deleted commented lines

--- Plasticity.jl
+++ Plasticity.jl
@@ -1,22 +1,21 @@
 using Waveforms
 using Plots
 
-# Helper function to get a triangle wave strain time history
 function get_cycles(; peak2peak=1, num_cycles=1, num_steps_per_cycle=100)
     return peak2peak * trianglewave1.(range(0.0, stop=num_cycles, length=num_steps_per_cycle*num_cycles))
 end
 
 ϵ = get_cycles(peak2peak=0.3/100, num_cycles=1, num_steps_per_cycle=1000)
 σₒ = 250.0
-E = 200000.0
-H_iso = 10000.0
-H_kin = 40000.0
+E = 200.0e3
+H_iso = 10.0e3
+H_kin = 40.0e3
 
 # Function for isotropic hardening
 function Isotropic_Hardening(ϵ, σₒ, E, H_iso)
     σ = zeros(length(ϵ))
     εₚ = 0.0
-    σᵧ = σₒ # Yield stress
+    σᵧ = σₒ 
 
     for i in 1:length(ϵ)
         εₑ = ϵ[i] - εₚ
@@ -38,7 +37,7 @@ end
 function Kinematic_Hardening(ϵ, σₒ, E, H_kin)
     σ = zeros(length(ϵ))
     εₚ = 0.0
-    σᵧ = σₒ # Yield stress
+    σᵧ = σₒ 
     α = 0.0
 
     for i in 1:length(ϵ)
@@ -61,7 +60,7 @@ end
 function Combined_Hardening(ϵ, σₒ, E, H_iso, H_kin)
     σ = zeros(length(ϵ))
     εₚ = 0.0
-    σᵧ = σₒ # Yield stress
+    σᵧ = σₒ 
     α = 0.0
 
     for i in 1:length(ϵ)
@@ -81,13 +80,11 @@ function Combined_Hardening(ϵ, σₒ, E, H_iso, H_kin)
 return σ
 end
 
-# σ_iso = Isotropic_Hardening(ϵ, σₒ, E, H_iso)
-# σ_kin = Kinematic_Hardening(ϵ, σₒ, E, H_kin)
-σ_com_pla = Combined_Hardening(ϵ, σₒ, E, 0.0, 0.0)
+σ_pp = Combined_Hardening(ϵ, σₒ, E, 0.0, 0.0)
 σ_iso = Combined_Hardening(ϵ, σₒ, E, H_iso, 0.0)
 σ_kin = Combined_Hardening(ϵ, σₒ, E, 0.0, H_kin)
 σ_com = Combined_Hardening(ϵ, σₒ, E, H_iso, H_kin)
-plot(ϵ, σ_com_pla, label="Perfect Plasticity", xlabel="ϵ", ylabel="σ", title="σ vs ϵ")
+plot(ϵ, σ_pp, label="Perfect Plasticity", xlabel="ϵ", ylabel="σ", title="σ vs ϵ")
 plot!(ϵ, σ_iso, label="Isotropic", xlabel="ϵ", ylabel="σ", title="σ vs ϵ")
 plot!(ϵ, σ_kin, label="Kinematic", xlabel="ϵ", ylabel="σ", title="σ vs ϵ")
 plot!(ϵ, σ_com, label="Combined", xlabel="ϵ", ylabel="σ", title="σ vs ϵ")
